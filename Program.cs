2270d9fcd90abde39869113ffcd16622c31216b0
FJ05 <48802078+FJ05@users.noreply.github.com>
2022-03-02 14:15:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Jul_skit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //fråga användaren hur många trianglar trädet ska ha
+            Console.WriteLine("Välj antalet trianglar i ditt jul träd: ");
+            int antal = int.Parse(Console.ReadLine());
+
+            //loopar för antal trianglar som ska bildas
+            for (int i = 1; i <= antal; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    //branch ska bli "*" j antal gånger
+                    string branch = new String('*', j);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    //skriver ut så att det bildas en triangel
+                    Console.WriteLine(branch.PadLeft(antal + 3) + "*" + branch);
+                    
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
